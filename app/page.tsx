6617eb1b97e4592db107600114488280a8d7758f
gateremark <mark5gatere@gmail.com>
2023-11-30 02:31:24 +0300

    change body gradient

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { FaArrowRight } from "react-icons/fa";
 
 const Home: React.FC = () => {
     return (
-        <main className=" h-fit pt-4 pb-6 md:px-8 px-4 flex flex-col items-center">
+        <main className=" h-fit pt-4 pb-6 md:px-8 px-4 flex flex-col items-center bg-gradient-to-b from-[#d6dbdc] to-[#ffffff]">
             <header className=" w-full h-[75px] flex items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] before:-z-20">
                 <Nav />
             </header>
